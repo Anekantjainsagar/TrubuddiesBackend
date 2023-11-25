307fb85f3815250a9e4ebeb65caee65fe65fa5fc
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2023-11-25 11:55:38 +0530

    Added jwt

--- Routes/Admin/admin.js
+++ Routes/Admin/admin.js
@@ -8,6 +8,7 @@ const Message = require("../../model/messageSchema");
 const Admin = require("../../model/adminSchema");
 
 const { validateSingin } = require("../../middlewares/auth");
+const jwt = require("jsonwebtoken");
 
 admin.post("/login", async (req, res) => {
   let { email, password } = req.body;
