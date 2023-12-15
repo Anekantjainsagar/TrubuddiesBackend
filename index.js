033a90a1cc7dabf20fc0e07fb20f32009fc8d007
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2023-12-15 22:36:11 +0530

    ASDF

--- index.js
+++ index.js
@@ -143,12 +143,12 @@ io.on("connection", (socket) => {
   socket.on("user-chat", async ({ from, id, message, profile, name, _id }) => {
     try {
       let chat = { sender: from, message, profile, name };
-
+      console.log({ from, id, message, profile, name, _id });
       let response = await GroupChat.updateOne(
         { _id: id, "chats._id": _id },
         { $push: { "chats.$.messages": chat } }
       );
-
+      console.log(response);
       io.local.emit("chat", chat);
     } catch (errors) {
       console.log(errors);
