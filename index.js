0dc564a1c7e52b41393255d6783c4ddc6addfaa7
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2024-01-09 12:42:59 +0530

    Credentials config

--- index.js
+++ index.js
@@ -45,6 +45,14 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "http://your-client-domain.com");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(express.json());
 
 connect();
