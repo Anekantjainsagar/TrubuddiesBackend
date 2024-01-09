1dc981e0d2902bb7c15ef3f7bb14dcc1225dc679
Anekant jain <94368450+Anekantjainsagar@users.noreply.github.com>
2024-01-09 13:00:41 +0530

    Added jwt in google

--- index.js
+++ index.js
@@ -120,7 +120,16 @@ app.get(
 app.get("/login/sucess", async (req, res) => {
   console.log(req.user);
   if (req.user) {
-    res.status(200).json({ message: "user Login", user: req.user });
+    const jwtToken = jwt.sign(
+      {
+        user: req.user._id,
+      },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1d",
+      }
+    );
+    res.status(200).json({ message: "user Login", user: req.user, jwtToken });
   } else {
     res.status(400).json({ message: "Not Authorized" });
   }
