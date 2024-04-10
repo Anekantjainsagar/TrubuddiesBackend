e6e51741ca2393e8ade0f461ffda8168a9ed8ed1
Anekantjainsagar <anekantjainsagar@gmail.com>
2024-04-10 17:23:37 +0530

    Err console

--- Routes/User/tokens.js
+++ Routes/User/tokens.js
@@ -52,7 +52,7 @@ tokens.post("/place", async (req, res) => {
         })
         .catch((err) => {
           console.log(err);
-          res.status(500).send(err.message);
+          res.status(500).send(err);
         });
     })
     .catch((err) => {
